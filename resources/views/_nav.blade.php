62c7d6a8ccac0d308aa468ad13fe9db2e19def41
denliedesign <customdenlie@gmail.com>
2024-11-19 10:15:46 -0600

    discounts in nav

--- resources/views/_nav.blade.php
+++ resources/views/_nav.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-warm">
     <div class="d-flex justify-content-center align-items-center text-center pt-3" style="font-size: 24px;">
         <ion-icon name="gift" style="color: #B12729;"></ion-icon>&nbsp;
-        <strong>Black Friday Sale:</strong>&nbsp;Lock in Your Spot Now to Get Your Website Live Before Christmas! Only 2 Openings Left – Act Quickly
+        <strong>Black Friday Sale:</strong>&nbsp;Lock in Your Spot Now to Get Your Website Live Before Christmas! Only&nbsp;<span style="color: #B12729;">2 Openings Left</span>&nbsp;– Act Quickly
     </div>
     <hr class="my-3">
     <nav class="navbar navbar-expand-lg bg-warm">
@@ -22,9 +22,18 @@
                     <li class="nav-item"><a class="nav-link text-dark mx-3" href="/#about">About</a></li>
                     <li class="nav-item"><a class="nav-link text-dark mx-3" href="/#testimonials">Testimonials</a></li>
 {{--                    &nbsp;<span style="color: grey;">|</span>&nbsp;--}}
-                    <li class="nav-item"><a class="nav-link text-dark mx-3" href="/denlie-bundle"><em>Denlie Bundle</em></a></li>
-                    <li class="nav-item"><a class="nav-link text-dark mx-3" href="/custom-denlie"><em>Custom Denlie</em></a></li>
-                    <li class="nav-item"><a class="nav-link text-dark mx-3" href="/start-your-journey"><em>Pick & Choose</em></a></li>
+                    <li class="nav-item text-center">
+                        <span class="rounded fonts-robo" style="background: #B12729; color: white; padding: 2px 4px; font-weight: 700; font-size: 12px;">$50 off monthly</span>
+                        <a class="nav-link text-dark mx-3 py-0" href="/denlie-bundle"><em>Denlie Bundle</em></a>
+                    </li>
+                    <li class="nav-item text-center">
+                        <span class="rounded fonts-robo" style="background: #B12729; color: white; padding: 2px 4px; font-weight: 700; font-size: 12px;">$200 off monthly</span>
+                        <a class="nav-link text-dark mx-3 py-0" href="/custom-denlie"><em>Custom Denlie</em></a>
+                    </li>
+                    <li class="nav-item text-center">
+                        <span class="rounded fonts-robo" style="background: #B12729; color: white; padding: 2px 4px; font-weight: 700; font-size: 12px;">50% off everything</span>
+                        <a class="nav-link text-dark mx-3 py-0" href="/start-your-journey"><em>Pick & Choose</em></a>
+                    </li>
 {{--                    <li class="nav-item"><a class="nav-link text-dark mx-3" href="#">Maintenance</a></li>--}}
 {{--                    <li class="nav-item"><a class="nav-link text-dark mx-3" href="#">Portfolio</a></li>--}}
 {{--                    <li class="nav-item"><a class="nav-link text-dark mx-3" href="#">Pricing</a></li>--}}
